6752bafe950c92aa95c301b6e1a50bb6886830f9
danilovieira37 <danilovieira37@icloud.com>
2025-02-08 14:58:14 -0300

    add correcao exercicio 84

--- exercicios_lista_guana.py
+++ exercicios_lista_guana.py
@@ -150,70 +150,76 @@
 #     print('Sua expressão está errada!')
 
 # 84
-nome_peso: list = []
-lista_completa: list = []
-continuar: str = 's'
-maior_peso: float = float(0)
-menor_peso: float = float(0)
-while continuar.lower() == 's':
-    while True:
-        try:
-            nome: str = input('Nome: ')
-            nome_peso.append(nome)    
-            peso: float = float(input('Peso: '))
-            nome_peso.append(peso)
-            lista_completa.append(nome_peso[:])
-            nome_peso.clear()
-            break
-        except ValueError:
-            print('Você não digitou nenhum nome ou peso, ou digitou uma letra no campo peso, por favor tente novamente.')
-    continuar: str = input('Quer continuar: [S/N]: ').strip()
-    while continuar not in ['S', 's', 'N', 'n']:
-        print('Opção inválida. Por favor, digite "S" para Sim ou "N" para não.')
-        continuar: str = input('Quer continuar: [S/N]: ').strip()
-## lista_completa = [['Maria', 70], ['João', 100], ['Claudio', 100]]
-for p, v in enumerate(lista_completa):    
-    if p == 0:
-        maior_peso = menor_peso = lista_completa[0][1]
-    else:
-        if v[1] > maior_peso:
-            maior_peso: float = float(v[1])
-        if v[1] < menor_peso:
-            menor_peso: float = float(v[1])        
-print('-=' * 30)
-print(f'Ao todo, você cadastrou {len(lista_completa[0])} pessoas.')
-print(f'O maior peso foi de {maior_peso}Kg. Peso de ', end='')
-for p, v in enumerate(lista_completa):    
-    if v[1] == maior_peso:
-        print(f'{v[0]}', end='... ')
-print()
-print(f'O menor peso foi de {menor_peso}Kg. Peso de ', end='')
-for p, v in enumerate(lista_completa):
-    if v[1] == menor_peso:
-        print(f'{v[0]}', end='... ')
-        
-
-# listanum = []
-# maior: int = int(0)
-# menor: int = int(0)
-# for c in range(0, 5):
-#     listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
-#     if c == 0:
-#         maior = menor = listanum[c]
+# nome_peso: list = []
+# lista_completa: list = []
+# continuar: str = 's'
+# maior_peso: float = float(0)
+# menor_peso: float = float(0)
+# while continuar.lower() == 's':
+#     while True:
+#         try:
+#             nome: str = input('Nome: ')
+#             nome_peso.append(nome)    
+#             peso: float = float(input('Peso: '))
+#             nome_peso.append(peso)
+#             lista_completa.append(nome_peso[:])
+#             nome_peso.clear()
+#             break
+#         except ValueError:
+#             print('Você não digitou nenhum nome ou peso, ou digitou uma letra no campo peso, por favor tente novamente.')
+#     continuar: str = input('Quer continuar: [S/N]: ').strip()
+#     while continuar not in ['S', 's', 'N', 'n']:
+#         print('Opção inválida. Por favor, digite "S" para Sim ou "N" para não.')
+#         continuar: str = input('Quer continuar: [S/N]: ').strip()
+# ## lista_completa = [['Maria', 70], ['João', 100], ['Claudio', 100]]
+# for p, v in enumerate(lista_completa):    
+#     if p == 0:
+#         maior_peso = menor_peso = lista_completa[0][1]
 #     else:
-#         if listanum[c] > maior:
-#             maior = listanum[c]
-#         if listanum[c] < menor:
-#             menor = listanum[c]
-# print('=-' * 30)
-# print(f'Você digitou os valores {listanum}')
-# print(f'O maior valor digitado foi {maior} nas posições ', end='')
-# for i, v in enumerate(listanum):
-#     if listanum[i] == maior:
-#         print(f'{i}...', end='')
+#         if v[1] > maior_peso:
+#             maior_peso: float = float(v[1])
+#         if v[1] < menor_peso:
+#             menor_peso: float = float(v[1])        
+# print('-=' * 30)
+# print(f'Ao todo, você cadastrou {len(lista_completa[0])} pessoas.')
+# print(f'O maior peso foi de {maior_peso}Kg. Peso de ', end='')
+# for p, v in enumerate(lista_completa):    
+#     if v[1] == maior_peso:
+#         print(f'{v[0]}', end='... ')
 # print()
-# print(f'O maior valor digitado foi {menor} nas posições ', end='')
-# for i, v in enumerate(listanum):
-#     if listanum[i] == menor:
-#         print(f'{i}...', end='')
-# print()
+# print(f'O menor peso foi de {menor_peso}Kg. Peso de ', end='')
+# for p, v in enumerate(lista_completa):
+#     if v[1] == menor_peso:
+#         print(f'{v[0]}', end='... ')
+        
+# Correção 84
+temp: list = []
+princ: list = []
+mai = men = 0
+
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp: str = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Ao todo, você cadastrou {len(princ)} pessoas. ')
+print(f'O maior peso foi de {mai}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}]', end=' ')
+print()
+print(f'O menor peso foi de {men}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}]', end=' ')
