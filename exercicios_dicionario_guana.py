b2bcc66f023cd82e855531675572a50f668c8460
unknown <danilovieira37@icloud.com>
2025-04-06 16:32:26 -0300

    add exercicio 95

--- exercicios_dicionario_guana.py
+++ exercicios_dicionario_guana.py
@@ -119,41 +119,74 @@
 # print(f'Foi um total de {jogador["total"]} gols.')
 
 # correcao ex 94
-galera = list()
-pessoa = dict()
-soma = media = 0
+# galera = list()
+# pessoa = dict()
+# soma = media = 0
+# while True:
+#     pessoa.clear()
+#     pessoa['nome'] = str(input('Nome: '))
+#     while True:
+#         pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+#         if pessoa['sexo'] in 'MF':
+#             break
+#         print('ERRO! Por favor, digite apenas M ou F.')
+#     pessoa['idade'] = int(input('Idade: '))
+#     soma+=pessoa['idade']
+#     galera.append(pessoa.copy())
+#     while True:
+#         resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+#         if resp in 'SN':
+#             break
+#         print('ERRO! Responda apenas S ou N.')
+#     if resp == 'N':
+#         break
+# print('-=' * 60)
+# print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+# media = soma / len(galera)
+# print(f'B) A média de idade é de {media:5.2f} anos.')
+# print(f'C) As mulheres cadastradas foram ', end='')
+# for p in galera:
+#     if p['sexo'] in 'Ff':
+#         print(f'{p["nome"]} ', end='')
+# print()
+# print(f'D) Lista das pessoas que estão acima da média: ', end='')
+# for p in galera:
+#     if p['idade'] >= media:
+#         print('   ')
+#         for k, v in p.items():
+#             print(f'{k} = {v}; ', end='')
+#         print()
+# print('<< ENCERRADO >>')
+
+# ex 95
+time = list()
+jogador = dict()
+partidas = list()
 while True:
-    pessoa.clear()
-    pessoa['nome'] = str(input('Nome: '))
-    while True:
-        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
-        if pessoa['sexo'] in 'MF':
-            break
-        print('ERRO! Por favor, digite apenas M ou F.')
-    pessoa['idade'] = int(input('Idade: '))
-    soma+=pessoa['idade']
-    galera.append(pessoa.copy())
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
+    for c in range(0, tot):
+        partidas.append(int(input(f'        Quantos gols na partida {c}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
     while True:
         resp = str(input('Quer continuar? [S/N] ')).upper()[0]
         if resp in 'SN':
             break
         print('ERRO! Responda apenas S ou N.')
     if resp == 'N':
-        break
-print('-=' * 60)
-print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
-media = soma / len(galera)
-print(f'B) A média de idade é de {media:5.2f} anos.')
-print(f'C) As mulheres cadastradas foram ', end='')
-for p in galera:
-    if p['sexo'] in 'Ff':
-        print(f'{p["nome"]} ', end='')
+        break    
+print('-=' * 30)
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
 print()
-print(f'D) Lista das pessoas que estão acima da média: ', end='')
-for p in galera:
-    if p['idade'] >= media:
-        print('   ')
-        for k, v in p.items():
-            print(f'{k} = {v}; ', end='')
-        print()
-print('<< ENCERRADO >>')
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
